3d2c0a7bf63221f5c4961371bb325b8f0fd3b544
LazyGalaxy <vangos@lazygalaxy.com>
2025-03-02 09:48:23 +0100

    doing transition to a dictionary so multiple reference files can be generated

--- gameinfo/generate_mame_xml.py
+++ gameinfo/generate_mame_xml.py
@@ -1,10 +1,12 @@
 import secret
 import xml.etree.cElementTree as ET
 import xml.dom.minidom
+import csv
 
 m_encoding = "UTF-8"
 
 try:
+    # get the gameinfo from snowflake
     curs = secret.conn.cursor()
     curs.execute(
         "select c.name, c.description, COALESCE(m.cloneof,m2.cloneof), c.year, COALESCE(m.year,m2.year), c.players, COALESCE(m.players,m2.players), c.ctrltype, c.manufacturer, COALESCE(m.manufacturer,m2.manufacturer),"
@@ -16,49 +18,71 @@ try:
         + "WHERE c.name in (select file from COINOPS_DELUXE_MAX_ROMS) OR c.year in ('Swap','theme') order by c.year,c.name ASC"
     )
 
-    # Get column names from cursor.description
+    # get column names from cursor.description
     columns = [desc[0] for desc in curs.description]
-    menu = ET.Element("menu")
 
-    # Iterate through each row and create XML elements
+    gameinfo = {}
+    # iterate through each sql result row and add the info in a dictionary
+    menuElement = ET.Element("menu")
     for row in curs.fetchall():
-        game = ET.SubElement(menu, "game", name=row[0])
+        game = row[0]
+        gameinfo[game] = {}
+        gameElement = ET.SubElement(menuElement, "game", name=game)
+
         if row[1]:
-            ET.SubElement(game, "description").text = row[1]
+            gameinfo[game]["description"] = row[1]
+            ET.SubElement(gameElement, "description").text = row[1]
         if row[2]:
-            ET.SubElement(game, "cloneof").text = row[2]
+            gameinfo[game]["cloneof"] = row[2]
+            ET.SubElement(gameElement, "cloneof").text = row[2]
         if row[4]:
-            ET.SubElement(game, "year").text = row[4]
+            gameinfo[game]["year"] = row[4]
+            ET.SubElement(gameElement, "year").text = row[4]
         elif row[3]:
-            ET.SubElement(game, "year").text = row[3]
+            gameinfo[game]["year"] = row[3]
+            ET.SubElement(gameElement, "year").text = row[3]
         if row[6] and not row[6]=='0':
-            ET.SubElement(game, "players").text = row[6]
+            gameinfo[game]["players"] = row[6]
+            ET.SubElement(gameElement, "players").text = row[6]
         elif row[5]:
-            ET.SubElement(game, "players").text = row[5]
+            gameinfo[game]["players"] = row[5]
+            ET.SubElement(gameElement, "players").text = row[5]
         if row[7]:
-            ET.SubElement(game, "ctrltype").text = (
-                row[7].replace("  ", " ").replace("  ", " ").replace("  ", " ")
-            )
+            ctrltype = row[7].replace("  ", " ").replace("  ", " ").replace("  ", " ")
+            gameinfo[game]["ctrltype"] = ctrltype
+            ET.SubElement(gameElement, "ctrltype").text = ctrltype
         if row[8] and not row[8].lower()=="other":
-            ET.SubElement(game, "manufacturer").text = row[8]
+            gameinfo[game]["manufacturer"] = row[8]
+            ET.SubElement(gameElement, "manufacturer").text = row[8]
         elif row[9]:
-            ET.SubElement(game, "manufacturer").text = row[9]
+            gameinfo[game]["manufacturer"] = row[9]
+            ET.SubElement(gameElement, "manufacturer").text = row[9]
         if row[10]:
-            ET.SubElement(game, "category").text = row[10]
+            gameinfo[game]["category"] = row[10]
+            ET.SubElement(gameElement, "category").text = row[10]
         if row[3] and not row[3]=='Swap' and not row[3]=='theme':
             if row[11] and (row[11] == "90" or row[11] == "270"):
-                ET.SubElement(game, "orientation").text = "vertical"
+                gameinfo[game]["orientation"] = "vertical"
+                ET.SubElement(gameElement, "orientation").text = "vertical"
             else:
-                ET.SubElement(game, "orientation").text = "horizontal"
+                gameinfo[game]["orientation"] = "horizontal"
+                ET.SubElement(gameElement, "orientation").text = "horizontal"
         if row[12]:
-            ET.SubElement(game, "ctrltype2").text = row[12]
+            gameinfo[game]["ctrltype2"] = row[12]
+            ET.SubElement(gameElement, "ctrltype2").text = row[12]
         if row[13]:
-            ET.SubElement(game, "joyways").text = row[13]
+            gameinfo[game]["joyways"] = row[13]
+            ET.SubElement(gameElement, "joyways").text = row[13]
         if row[14]:
-            ET.SubElement(game, "buttons").text = row[14]
+            gameinfo[game]["buttons"] = row[14]
+            ET.SubElement(gameElement, "buttons").text = row[14]
+
+    # iterate through the dictionary and create the xml and csv file
+    for key in gameinfo:
+        print(key, "->", gameinfo[key])
 
     # save the .xml
-    dom = xml.dom.minidom.parseString(ET.tostring(menu))
+    dom = xml.dom.minidom.parseString(ET.tostring(menuElement))
     xml_string = dom.toprettyxml()
     part1, part2 = xml_string.split("?>")
 
